54aea5417e2ca71e391d7c80e7c3a4c2f8a6d079
rohitkumar0134 <rohit54pandit@gmail.com>
2023-03-15 22:27:10 +0530

    Added user and static files

--- movierater/settings.py
+++ movierater/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dj_database_url import parse as dburl
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,11 +84,10 @@ WSGI_APPLICATION = 'movierater.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+default_dburl='sqlite:///'+ os.path.join(BASE_DIR,'db.sqlite3')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': config('DATABASE_URL',default=default_dburl,cast=dburl)
 }
 
 REST_FRAMEWORK = {
@@ -137,5 +138,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    (os.path.join(BASE_DIR, 'static'))
 ]
