3db68d0b5ea62c77104e62d0b3960eb49514310c
Rohitkumar Pandit <rohit54pandit@gmail.com>
2023-03-16 11:13:11 +0530

    Update settings.py

--- movierater/settings.py
+++ movierater/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_WHITELIST = (
-    'http://localhost:3000', 'htpp://localhost:4200', 'https://movie-rater-react.netlify.app','https://64121d20b13316040629c3c4--movie-rater-react.netlify.app','https://main--movie-rater-react.netlify.app/',
+    'http://localhost:3000', 'htpp://localhost:4200', 'https://movie-rater-react.netlify.app','https://main--movie-rater-react.netlify.app',
 )
 
 ROOT_URLCONF = 'movierater.urls'
